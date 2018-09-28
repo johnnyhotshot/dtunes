fd76179b26978d9aeeb7b85d1e05afa6f121b224
johnnyhotshot <johnnypuskar@yahoo.com>
2018-09-27 20:01:31 -0400

    Transferring javascript to player.js

--- js/player.js
+++ js/player.js
@@ -2,31 +2,37 @@ var spreadsheetUrl = "https://docs.google.com/spreadsheets/d/15_NsP5AlkPqzaT2CEZ
 
 var videoIDs = [];
 
-/*
+var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          videoId: videoIDs[0],
+          events: {
+            'onReady': onPlayerReady
+          }
+        });
+      }
 
-		<script src='https://cdn.rawgit.com/jsoma/tabletop/master/src/tabletop.min.js'></script>
-		<script src="js/player.js"></script>
-*/
+      function onPlayerReady(event) {
+        event.target.playVideo();
+      }
 
 function main() {
 	console.log(videoIDs);
-	
-	
 }
 
 function init() {
   Tabletop.init( { key: spreadsheetUrl,
-					callback: function(data, tabletop) { 
-						for (var i = 0; i < data.length; i++) {
-							let link = data[i]["LINK 1"];
-							if(link.indexOf("youtube.com") != -1) {
-								videoIDs.push(link.substring(link.indexOf("v=")+2));
-							}
-						}
-						
-						main();
-					},
-					simpleSheet: true } );
+    callback: function(data, tabletop) { 
+        for (var i = 0; i < data.length; i++) {
+            let link = data[i]["LINK 1"];
+            if(link.indexOf("youtube.com") != -1) {
+                videoIDs.push(link.substring(link.indexOf("v=")+2));
+            }
+        }
+
+        main();
+    },
+    simpleSheet: true } );
 }
 
 window.addEventListener('DOMContentLoaded', init);
