61d2254be30b3ff18001f24eb78fc15559918565
johnnyhotshot <johnnypuskar@yahoo.com>
2018-09-27 20:17:45 -0400

    Player will now select a random video to switch to when the current one ends

--- js/player.js
+++ js/player.js
@@ -2,37 +2,54 @@ var spreadsheetUrl = "https://docs.google.com/spreadsheets/d/15_NsP5AlkPqzaT2CEZ
 
 var videoIDs = [];
 
-var player;
-      function onYouTubeIframeAPIReady() {
-        player = new YT.Player('player', {
-          videoId: videoIDs[0],
-          events: {
-            'onReady': onPlayerReady
-          }
-        });
-      }
+var apiReady = false, spreadsheetReady = false;
 
-      function onPlayerReady(event) {
-        event.target.playVideo();
-      }
+var player;
+function onYouTubeIframeAPIReady() {
+    apiReady = true;
+    main();
+}
+
+function setupPlayer() {
+    player = new YT.Player('player', {
+        videoId: videoIDs[0],
+        events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+        }
+    });
+}
+
+function onPlayerReady(event) {
+    event.target.playVideo();
+}
+
+function onPlayerStateChange(event) {
+    if(event.player.getPlayerState() == 0) {
+        event.player.loadVideoById(videoIDs[Math.floor(Math.random()*videoIDs.length)]);
+    }
+}
 
 function main() {
-	console.log(videoIDs);
+    if(apiReady && spreadsheetReady) {
+        setupPlayer();
+        console.log(videoIDs);
+    }
 }
 
 function init() {
-  Tabletop.init( { key: spreadsheetUrl,
-    callback: function(data, tabletop) { 
-        for (var i = 0; i < data.length; i++) {
-            let link = data[i]["LINK 1"];
-            if(link.indexOf("youtube.com") != -1) {
-                videoIDs.push(link.substring(link.indexOf("v=")+2));
+    Tabletop.init( { key: spreadsheetUrl,
+        callback: function(data, tabletop) { 
+            for (var i = 0; i <     data.length; i++) {
+                let link = data[i]["LINK 1"];
+                if(link.indexOf("youtube.com") != -1) {
+                    videoIDs.push(link.substring(link.indexOf("v=")+2));
+                }
             }
-        }
-
-        main();
-    },
-    simpleSheet: true } );
+            spreadsheetReady = true;
+            main();
+        },
+        simpleSheet: true } );
 }
 
 window.addEventListener('DOMContentLoaded', init);
